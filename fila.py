ce15500404f38b11760a5294b2190dbc8057e20c
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-12-22 16:05:07 -0500

    Updated from Brython Server: 12/22/2018 4:05:07 PM

--- fila.py
+++ fila.py
@@ -1,1 +1,6 @@
+x = input("which part of the cytoskeleton are you confused about? ")
+y = input("function or structure? ")
 
+while x == "microfilament":
+    if y == "function:
+        print("Provides support for the cell
