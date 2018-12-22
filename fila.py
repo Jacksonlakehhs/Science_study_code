962377c635cc65dc2cb700a3d4e10fd9e6135712
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-12-22 16:17:43 -0500

    Updated from Brython Server: 12/22/2018 4:17:43 PM

--- fila.py
+++ fila.py
@@ -6,3 +6,8 @@ while x == "microfilament":
         print("Provides support for the cell and is involved with cell movement.")
     elif y == "structure":
         print("Composed of actin arranged in twisted double chain.")
+
+while x == "microtubule":
+    if y == "function:
+        print("Shape and support the cell. They also act as tracks for organelles with motor proteins to move":
+        print("Composed of actin arranged in twisted double chain.")
