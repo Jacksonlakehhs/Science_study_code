70f6eb7376f10e4b592a47aeaee0f1069cf5f583
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-12-22 16:24:28 -0500

    Updated from Brython Server: 12/22/2018 4:24:28 PM

--- fila.py
+++ fila.py
@@ -2,12 +2,12 @@ x = input("which part of the cytoskeleton are you confused about? ")
 y = input("function or structure? ")
 
 while x == "microfilament":
-    if y == "function:
+    if y == "function":
         print("Provides support for the cell and is involved with cell movement.")
     elif y == "structure":
         print("Composed of actin arranged in twisted double chain.")
 
-while x == "microtubule":
-    if y == "function:
-        print("Shape and support the cell. They also act as tracks for organelles with motor proteins to move":
-        print("Composed of tubulin arranged in a straight, hollow tube.")
+#while x == "microtubule":
+#    if y == "function":
+#        print("Shape and support the cell. They also act as tracks for organelles with motor proteins to move":
+#        print("Composed of tubulin arranged in a straight, hollow tube.")
